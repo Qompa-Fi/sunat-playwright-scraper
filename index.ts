65d3430641c99e4e235e3e0e573e57e43cce249b
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-06 16:56:42 -0500

    refactor: added namespaces for scraper and sunat api

--- index.ts
+++ index.ts
@@ -58,13 +58,13 @@ const main = async () => {
     .get(
       "/sales-and-revenue-management",
       async ({ query: credentials, error }) => {
-        const token = await getSunatToken(browser, credentials);
+        const token = await Scraper.getSunatToken(browser, credentials);
         if (!token) {
           log.error("sunat token could not be retrieved");
           return error(500, "Internal Server Error");
         }
 
-        const data = await getExportedData(token, "080000");
+        const data = await SunatAPI.getExportedData(token, "080000");
         return data;
       },
       { query: credentialsSchema },
@@ -72,13 +72,13 @@ const main = async () => {
     .get(
       "/sales-and-revenue-management/periods",
       async ({ query: credentials, error }) => {
-        const token = await getSunatToken(browser, credentials);
+        const token = await Scraper.getSunatToken(browser, credentials);
         if (!token) {
           log.error("sunat token could not be retrieved");
           return error(500, "Internal Server Error");
         }
 
-        const periods = await getPeriods(token, "080000");
+        const periods = await SunatAPI.getPeriods(token, "080000");
         return {
           tax_periods: periods,
         };
@@ -88,13 +88,13 @@ const main = async () => {
     .get(
       "/purchasing-management",
       async ({ query: credentials, error }) => {
-        const token = await getSunatToken(browser, credentials);
+        const token = await Scraper.getSunatToken(browser, credentials);
         if (!token) {
           log.error("sunat token could not be retrieved");
           return error(500, "Internal Server Error");
         }
 
-        const data = await getExportedData(token, "140000");
+        const data = await SunatAPI.getExportedData(token, "140000");
         return data;
       },
       { query: credentialsSchema },
@@ -102,13 +102,13 @@ const main = async () => {
     .get(
       "/purchasing-management/periods",
       async ({ query: credentials, error }) => {
-        const token = await getSunatToken(browser, credentials);
+        const token = await Scraper.getSunatToken(browser, credentials);
         if (!token) {
           log.error("sunat token could not be retrieved");
           return error(500, "Internal Server Error");
         }
 
-        const periods = await getPeriods(token, "140000");
+        const periods = await SunatAPI.getPeriods(token, "140000");
         return {
           tax_periods: periods,
         };
@@ -136,229 +136,243 @@ export type PeriodResult = {
   tax_periods: string[];
 };
 
-const getSunatToken = async (
-  browser: Browser,
-  credentials: SunatCredentials,
-): Promise<string | null> => {
-  const page = await browser.newPage();
-  await page.goto("https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm", {
-    waitUntil: "networkidle",
-  });
-
-  page.pause();
-
-  log.debug("handling login...");
-  await handleLogin(page, credentials);
-
-  const title = await page.title();
-  if (title !== "SUNAT - Menú SOL") {
-    throw new Error(`Unexpected page title: ${title}`);
-  }
-
-  log.debug("mitigating possible redundant menu items...");
-  await mitigateRedundantMenuItems(page);
-
-  log.debug("navigating to electronic sales and revenue management...");
-  await menuToElectronicSalesAndRevenueManagement(page);
-
-  const anyFrame = page.frame({ url: /ww1.sunat.gob.pe/ });
-  if (!anyFrame) {
-    throw new Error("frame for ww1.sunat.gob.pe was not found");
-  }
-
-  await anyFrame.goto("https://e-factura.sunat.gob.pe");
-  await anyFrame.waitForLoadState("networkidle");
-
-  const sunatToken = await anyFrame.evaluate(async () =>
-    window.sessionStorage.getItem("SUNAT.token"),
-  );
-  await page.close();
-
-  if (!sunatToken) {
-    return null;
-  }
-
-  return sunatToken;
-};
-
-const getPeriods = async (
-  sunatToken: string,
-  bookCode: string,
-): Promise<string[]> => {
-  const response = await fetch(
-    `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/padron/web/omisos/${bookCode}/periodos`,
-    {
-      credentials: "include",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
-        Accept: "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        authorization: `Bearer ${sunatToken}`,
-        "Sec-GPC": "1",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
+namespace SunatAPI {
+  export const getPeriods = async (
+    sunatToken: string,
+    bookCode: string,
+  ): Promise<string[]> => {
+    const response = await fetch(
+      `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/padron/web/omisos/${bookCode}/periodos`,
+      {
+        credentials: "include",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+          Accept: "application/json, text/plain, */*",
+          "Accept-Language": "en-US,en;q=0.5",
+          authorization: `Bearer ${sunatToken}`,
+          "Sec-GPC": "1",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-site",
+        },
+        referrer: "https://e-factura.sunat.gob.pe/",
+        method: "GET",
+        mode: "cors",
       },
-      referrer: "https://e-factura.sunat.gob.pe/",
-      method: "GET",
-      mode: "cors",
-    },
-  );
-
-  interface RawPeriod {
-    numEjercicio: string;
-    desEstado: string;
-    lisPeriodos: Array<{
-      perTributario: string;
-      codEstado: string;
-      desEstado: string;
-    }>;
-  }
-
-  const untypedData = await response.json();
-  const data = untypedData as RawPeriod[];
-
-  const taxPeriods: Array<string> = [];
-
-  for (const item of data) {
-    for (const period of item.lisPeriodos) {
-      taxPeriods.push(period.perTributario);
-    }
-  }
-
-  return taxPeriods;
-};
-
-const getExportedData = async (
-  sunatToken: string,
-  bookCode: BookCode,
-): Promise<string> => {
-  const response = await fetch(
-    `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/resumen/web/resumencomprobantes/202412/1/1/exporta?codLibro=${bookCode}`,
-    {
-      credentials: "include",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
-        Accept: "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        authorization: `Bearer ${sunatToken}`,
-        "Sec-GPC": "1",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        Priority: "u=0",
-      },
-      referrer: "https://e-factura.sunat.gob.pe/",
-      method: "GET",
-      mode: "cors",
-    },
-  );
-
-  return await response.text();
-};
-
-const menuToElectronicSalesAndRevenueManagement = async (menuPage: Page) => {
-  const must$ = async (selector: string) => await pageMust$(menuPage, selector);
-
-  const service2Option = await must$("#divOpcionServicio2");
-  await service2Option.click();
-
-  const SIRE_LABEL = "Sistema Integrado de Registros Electronicos";
-  const sireOption = menuPage.getByText(SIRE_LABEL);
-  await sireOption.click();
-
-  const sireElectronicRecords = await must$(
-    "#nivel1Cuerpo_60 .nivel2 .spanNivelDescripcion", // AKA Registros Electronicos
-  );
-  await sireElectronicRecords.click();
-
-  await menuPage.getByText("Registro de Ventas e Ingresos Electronico").click();
-  await menuPage.getByText("Gestión de Ventas e Ingresos Electrónicos").click();
-
-  await menuPage.waitForLoadState("networkidle");
-
-  const appFrame = menuPage.frameLocator("#iframeApplication");
-  if (!appFrame) throw new Error("main frame not found");
-
-  const adviceModal = appFrame.getByText("×", { exact: true }); // if advice modal is open
-  const isAdviceModalOpen = await adviceModal.isVisible();
-  if (isAdviceModalOpen) await adviceModal.click();
-};
-
-const pageMust$ = async (
-  page: Page,
-  selector: string,
-): Promise<ElementHandle<SVGElement | HTMLElement>> => {
-  const element = await page.$(selector);
-  if (!element) throw new Error(`Element not found: ${selector}`);
-  return element;
-};
-
-const handleLogin = async (loginPage: Page, credentials: SunatCredentials) => {
-  const must$ = async (selector: string) =>
-    await pageMust$(loginPage, selector);
-
-  const rucInput = await must$("#txtRuc");
-  await rucInput.click();
-  await rucInput.fill(credentials.ruc);
-
-  const solUsernameInput = await must$("#txtUsuario");
-  await solUsernameInput.click();
-  await solUsernameInput.fill(credentials.sol_username);
-
-  const solKeyInput = await must$("#txtContrasena");
-  await solKeyInput.click();
-  await solKeyInput.fill(credentials.sol_key);
-
-  const submitButton = await must$("#btnAceptar");
-  await submitButton.click();
-};
-
-const mitigateRedundantMenuItems = async (menuPage: Page) => {
-  await menuPage.waitForLoadState("networkidle");
-
-  const campaignFrame = menuPage.frameLocator("#ifrVCE");
-
-  const secondaryModalLocator = campaignFrame.locator(
-    "#modalInformativoSecundario",
-  );
-  const SECONDARY_MODAL_TITLE = "Informativo";
-  const suggestionLocator = secondaryModalLocator.getByText(
-    SECONDARY_MODAL_TITLE,
-  );
-
-  let suggestionInnerText = "";
-
-  try {
-    suggestionInnerText = await suggestionLocator.innerText({
-      timeout: 3000,
-    });
-  } catch (error) {
-    log.debug(
-      "skipping mitigation...",
-      error instanceof Error ? error.message : error,
     );
-    return;
-  }
 
-  const secondaryModalExists =
-    suggestionInnerText.trim() === SECONDARY_MODAL_TITLE;
+    interface RawPeriod {
+      numEjercicio: string;
+      desEstado: string;
+      lisPeriodos: Array<{
+        perTributario: string;
+        codEstado: string;
+        desEstado: string;
+      }>;
+    }
 
-  if (secondaryModalExists) {
-    const submitButton = campaignFrame.getByRole("button", {
-      name: /Finalizar/i,
-    });
+    const untypedData = await response.json();
+    const data = untypedData as RawPeriod[];
+
+    const taxPeriods: Array<string> = [];
+
+    for (const item of data) {
+      for (const period of item.lisPeriodos) {
+        taxPeriods.push(period.perTributario);
+      }
+    }
+
+    return taxPeriods;
+  };
+
+  export const getExportedData = async (
+    sunatToken: string,
+    bookCode: BookCode,
+  ): Promise<string> => {
+    const response = await fetch(
+      `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/resumen/web/resumencomprobantes/202412/1/1/exporta?codLibro=${bookCode}`,
+      {
+        credentials: "include",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+          Accept: "application/json, text/plain, */*",
+          "Accept-Language": "en-US,en;q=0.5",
+          authorization: `Bearer ${sunatToken}`,
+          "Sec-GPC": "1",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-site",
+          Priority: "u=0",
+        },
+        referrer: "https://e-factura.sunat.gob.pe/",
+        method: "GET",
+        mode: "cors",
+      },
+    );
+
+    return await response.text();
+  };
+}
+
+namespace Scraper {
+  export const getSunatToken = async (
+    browser: Browser,
+    credentials: SunatCredentials,
+  ): Promise<string | null> => {
+    const page = await browser.newPage();
+    await page.goto(
+      "https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm",
+      {
+        waitUntil: "networkidle",
+      },
+    );
+
+    page.pause();
+
+    log.debug("handling login...");
+    await handleLogin(page, credentials);
+
+    const title = await page.title();
+    if (title !== "SUNAT - Menú SOL") {
+      throw new Error(`Unexpected page title: ${title}`);
+    }
+
+    log.debug("mitigating possible redundant menu items...");
+    await mitigateRedundantMenuItems(page);
+
+    log.debug("navigating to electronic sales and revenue management...");
+    await menuToElectronicSalesAndRevenueManagement(page);
+
+    const anyFrame = page.frame({ url: /ww1.sunat.gob.pe/ });
+    if (!anyFrame) {
+      throw new Error("frame for ww1.sunat.gob.pe was not found");
+    }
+
+    await anyFrame.goto("https://e-factura.sunat.gob.pe");
+    await anyFrame.waitForLoadState("networkidle");
+
+    const sunatToken = await anyFrame.evaluate(async () =>
+      window.sessionStorage.getItem("SUNAT.token"),
+    );
+    await page.close();
+
+    if (!sunatToken) {
+      return null;
+    }
+
+    return sunatToken;
+  };
+
+  const menuToElectronicSalesAndRevenueManagement = async (menuPage: Page) => {
+    const must$ = async (selector: string) =>
+      await pageMust$(menuPage, selector);
+
+    const service2Option = await must$("#divOpcionServicio2");
+    await service2Option.click();
+
+    const SIRE_LABEL = "Sistema Integrado de Registros Electronicos";
+    const sireOption = menuPage.getByText(SIRE_LABEL);
+    await sireOption.click();
+
+    const sireElectronicRecords = await must$(
+      "#nivel1Cuerpo_60 .nivel2 .spanNivelDescripcion", // AKA Registros Electronicos
+    );
+    await sireElectronicRecords.click();
+
+    await menuPage
+      .getByText("Registro de Ventas e Ingresos Electronico")
+      .click();
+    await menuPage
+      .getByText("Gestión de Ventas e Ingresos Electrónicos")
+      .click();
+
+    await menuPage.waitForLoadState("networkidle");
+
+    const appFrame = menuPage.frameLocator("#iframeApplication");
+    if (!appFrame) throw new Error("main frame not found");
+
+    const adviceModal = appFrame.getByText("×", { exact: true }); // if advice modal is open
+    const isAdviceModalOpen = await adviceModal.isVisible();
+    if (isAdviceModalOpen) await adviceModal.click();
+  };
+
+  const pageMust$ = async (
+    page: Page,
+    selector: string,
+  ): Promise<ElementHandle<SVGElement | HTMLElement>> => {
+    const element = await page.$(selector);
+    if (!element) throw new Error(`Element not found: ${selector}`);
+    return element;
+  };
+
+  const handleLogin = async (
+    loginPage: Page,
+    credentials: SunatCredentials,
+  ) => {
+    const must$ = async (selector: string) =>
+      await pageMust$(loginPage, selector);
+
+    const rucInput = await must$("#txtRuc");
+    await rucInput.click();
+    await rucInput.fill(credentials.ruc);
+
+    const solUsernameInput = await must$("#txtUsuario");
+    await solUsernameInput.click();
+    await solUsernameInput.fill(credentials.sol_username);
+
+    const solKeyInput = await must$("#txtContrasena");
+    await solKeyInput.click();
+    await solKeyInput.fill(credentials.sol_key);
+
+    const submitButton = await must$("#btnAceptar");
     await submitButton.click();
-  }
+  };
 
-  log.debug("skipping secondary modal...");
+  const mitigateRedundantMenuItems = async (menuPage: Page) => {
+    await menuPage.waitForLoadState("networkidle");
 
-  const skipButton = campaignFrame.getByText("Continuar sin confirmar");
+    const campaignFrame = menuPage.frameLocator("#ifrVCE");
 
-  await skipButton.click({ timeout: 3000 });
-};
+    const secondaryModalLocator = campaignFrame.locator(
+      "#modalInformativoSecundario",
+    );
+    const SECONDARY_MODAL_TITLE = "Informativo";
+    const suggestionLocator = secondaryModalLocator.getByText(
+      SECONDARY_MODAL_TITLE,
+    );
+
+    let suggestionInnerText = "";
+
+    try {
+      suggestionInnerText = await suggestionLocator.innerText({
+        timeout: 3000,
+      });
+    } catch (error) {
+      log.debug(
+        "skipping mitigation...",
+        error instanceof Error ? error.message : error,
+      );
+      return;
+    }
+
+    const secondaryModalExists =
+      suggestionInnerText.trim() === SECONDARY_MODAL_TITLE;
+
+    if (secondaryModalExists) {
+      const submitButton = campaignFrame.getByRole("button", {
+        name: /Finalizar/i,
+      });
+      await submitButton.click();
+    }
+
+    log.debug("skipping secondary modal...");
+
+    const skipButton = campaignFrame.getByText("Continuar sin confirmar");
+
+    await skipButton.click({ timeout: 3000 });
+  };
+}
 
 await main();
-
