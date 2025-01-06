293a0117814873f336bd50ea5ff1b704037ac804
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-06 16:52:48 -0500

    feat: added endpoints to get available data periods

--- index.ts
+++ index.ts
@@ -12,6 +12,8 @@ interface SunatCredentials {
   ruc: string;
 }
 
+type BookCode = "140000" | "080000";
+
 const credentialsSchema = t.Object({
   sol_username: t.String({ minLength: 3, maxLength: 8 }),
   sol_key: t.String({ minLength: 2, maxLength: 12 }),
@@ -55,18 +57,61 @@ const main = async () => {
     })
     .get(
       "/sales-and-revenue-management",
-      async ({ query: credentials }) => {
-        const result = await handle(browser, credentials, "080000");
-        return result;
+      async ({ query: credentials, error }) => {
+        const token = await getSunatToken(browser, credentials);
+        if (!token) {
+          log.error("sunat token could not be retrieved");
+          return error(500, "Internal Server Error");
+        }
+
+        const data = await getExportedData(token, "080000");
+        return data;
+      },
+      { query: credentialsSchema },
+    )
+    .get(
+      "/sales-and-revenue-management/periods",
+      async ({ query: credentials, error }) => {
+        const token = await getSunatToken(browser, credentials);
+        if (!token) {
+          log.error("sunat token could not be retrieved");
+          return error(500, "Internal Server Error");
+        }
+
+        const periods = await getPeriods(token, "080000");
+        return {
+          tax_periods: periods,
+        };
       },
       { query: credentialsSchema },
     )
     .get(
       "/purchasing-management",
-      async ({ query: credentials }) => {
-        const result = await handle(browser, credentials, "140000");
+      async ({ query: credentials, error }) => {
+        const token = await getSunatToken(browser, credentials);
+        if (!token) {
+          log.error("sunat token could not be retrieved");
+          return error(500, "Internal Server Error");
+        }
 
-        return result;
+        const data = await getExportedData(token, "140000");
+        return data;
+      },
+      { query: credentialsSchema },
+    )
+    .get(
+      "/purchasing-management/periods",
+      async ({ query: credentials, error }) => {
+        const token = await getSunatToken(browser, credentials);
+        if (!token) {
+          log.error("sunat token could not be retrieved");
+          return error(500, "Internal Server Error");
+        }
+
+        const periods = await getPeriods(token, "140000");
+        return {
+          tax_periods: periods,
+        };
       },
       { query: credentialsSchema },
     );
@@ -85,11 +130,16 @@ const main = async () => {
     );
 };
 
-const handle = async (
+// export type ExportResult = {};
+
+export type PeriodResult = {
+  tax_periods: string[];
+};
+
+const getSunatToken = async (
   browser: Browser,
   credentials: SunatCredentials,
-  bookCode: "140000" | "080000",
-) => {
+): Promise<string | null> => {
   const page = await browser.newPage();
   await page.goto("https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm", {
     waitUntil: "networkidle",
@@ -125,17 +175,64 @@ const handle = async (
   await page.close();
 
   if (!sunatToken) {
-    throw new Error("SUNAT.token not found");
+    return null;
   }
 
-  const summary = await getExportedData(sunatToken, bookCode);
+  return sunatToken;
+};
 
-  return summary;
+const getPeriods = async (
+  sunatToken: string,
+  bookCode: string,
+): Promise<string[]> => {
+  const response = await fetch(
+    `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/padron/web/omisos/${bookCode}/periodos`,
+    {
+      credentials: "include",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+        Accept: "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        authorization: `Bearer ${sunatToken}`,
+        "Sec-GPC": "1",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+      },
+      referrer: "https://e-factura.sunat.gob.pe/",
+      method: "GET",
+      mode: "cors",
+    },
+  );
+
+  interface RawPeriod {
+    numEjercicio: string;
+    desEstado: string;
+    lisPeriodos: Array<{
+      perTributario: string;
+      codEstado: string;
+      desEstado: string;
+    }>;
+  }
+
+  const untypedData = await response.json();
+  const data = untypedData as RawPeriod[];
+
+  const taxPeriods: Array<string> = [];
+
+  for (const item of data) {
+    for (const period of item.lisPeriodos) {
+      taxPeriods.push(period.perTributario);
+    }
+  }
+
+  return taxPeriods;
 };
 
 const getExportedData = async (
   sunatToken: string,
-  bookCode: "140000" | "080000",
+  bookCode: BookCode,
 ): Promise<string> => {
   const response = await fetch(
     `https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/resumen/web/resumencomprobantes/202412/1/1/exporta?codLibro=${bookCode}`,
@@ -265,23 +362,3 @@ const mitigateRedundantMenuItems = async (menuPage: Page) => {
 
 await main();
 
-/**
-   * Periods
-   * 
-  await fetch("https://api-sire.sunat.gob.pe/v1/contribuyente/migeigv/libros/rvierce/padron/web/omisos/140000/periodos", {
-    "credentials": "include",
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
-        "Accept": "application/json, text/plain, *\/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "authorization": "Bearer eyJraWQiOiJhcGkuc3VuYXQuZ29iLnBlLmtpZDAwMSIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyMDU2NjAzNjg5NSIsImF1ZCI6Ilt7XCJhcGlcIjpcImh0dHBzOlwvXC9hcGktc2lyZS5zdW5hdC5nb2IucGVcIixcInJlY3Vyc29cIjpbe1wiaWRcIjpcIlwvdjFcL2NvbnRyaWJ1eWVudGVcL21pZ2VpZ3ZcIixcImluZGljYWRvclwiOlwiMVwiLFwiZ3RcIjpcIjEwMDEwMFwifV19XSIsInVzZXJkYXRhIjp7Im51bVJVQyI6IjIwNTY2MDM2ODk1IiwidGlja2V0IjoiMTYxNDUyNTExMzYiLCJucm9SZWdpc3RybyI6IiIsImFwZU1hdGVybm8iOiIiLCJsb2dpbiI6IjIwNTY2MDM2ODk1R1JFSkVXSU4iLCJub21icmVDb21wbGV0byI6IlJFTkRFWiAtIFZPVVMgU09DSUVEQUQgQU5PTklNQSBDRVJSQURBIiwibm9tYnJlcyI6IlJFTkRFWiAtIFZPVVMgU09DSUVEQUQgQU5PTklNQSBDRVJSQURBIiwiY29kRGVwZW5kIjoiMDAyMyIsImNvZFRPcGVDb21lciI6IiIsImNvZENhdGUiOiIiLCJuaXZlbFVPIjowLCJjb2RVTyI6IiIsImNvcnJlbyI6IiIsInVzdWFyaW9TT0wiOiJHUkVKRVdJTiIsImlkIjoiIiwiZGVzVU8iOiIiLCJkZXNDYXRlIjoiIiwiYXBlUGF0ZXJubyI6IiIsImlkQ2VsdWxhciI6bnVsbCwibWFwIjp7ImlzQ2xvbiI6ZmFsc2UsImRkcERhdGEiOnsiZGRwX251bXJ1YyI6IjIwNTY2MDM2ODk1IiwiZGRwX251bXJlZyI6IjAwMjMiLCJkZHBfZXN0YWRvIjoiMDAiLCJkZHBfZmxhZzIyIjoiMDAiLCJkZHBfdWJpZ2VvIjoiMTUwMTQxIiwiZGRwX3RhbWFubyI6IjAyIiwiZGRwX3Rwb2VtcCI6IjM5IiwiZGRwX2NpaXUiOiI1NTIwNSJ9LCJpZE1lbnUiOiIxNjE0NTI1MTEzNiIsImpuZGlQb29sIjoicDAwMjMiLCJ0aXBVc3VhcmlvIjoiMCIsInRpcE9yaWdlbiI6IklUIiwicHJpbWVyQWNjZXNvIjpmYWxzZX19LCJuYmYiOjE3MzYxOTMxODUsImNsaWVudElkIjoiZTJmNzZmNWEtM2Y4MS00ZTg1LWE2ODEtYzc5YjVhNGJkNmE4IiwiaXNzIjoiaHR0cHM6XC9cL2FwaS1zZWd1cmlkYWQuc3VuYXQuZ29iLnBlXC92MVwvY2xpZW50ZXNzb2xcL2UyZjc2ZjVhLTNmODEtNGU4NS1hNjgxLWM3OWI1YTRiZDZhOFwvb2F1dGgyXC90b2tlblwvIiwiZXhwIjoxNzM2MTk2Nzg1LCJncmFudFR5cGUiOiJhdXRob3JpemF0aW9uX3Rva2VuIiwiaWF0IjoxNzM2MTkzMTg1fQ.eexo3xGYAoADIJ2KyfSUohvaKTxxl0OtAB5YL6_Gf7GFfseyDPbCqwrFLMw9RtYwqYUi4teibudjfW4pJO2FkBhKhy5-BeMlIKykKjqTal9iDYnWRaHcPqXlFtUrZqBjIOvnoVTR2BYJbrm2029TqolkytkT6EfmUNWOBDGcD_-kOyb11QZkGLggnncXR_K7HvxLVCu85HnyTCSbKcwh2Z09CfhgU-0XnRyJcsj76DMYKnwQh7tq9oG-CJFdsZqvVWSczPOZuBlQMx3yXVax7Q8xFLU23R8ToyyOxLTFKxWM9LHtuLA0x90CEPyV3aSLRukoHHSP-4hMJiSnFJlsZQ",
-        "Sec-GPC": "1",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site"
-    },
-    "referrer": "https://e-factura.sunat.gob.pe/",
-    "method": "GET",
-    "mode": "cors"
-});
-   */
